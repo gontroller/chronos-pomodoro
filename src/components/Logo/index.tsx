2ae609939ad4c72e5e796b8eed84ab14a5112910
Gon <gontrollerdev@gmail.com>
2025-07-30 16:05:29 -0300

    stylish logo added

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,13 @@
-import styles from './styles.module.css';
+import { TimerIcon } from "lucide-react";
+import styles from "./styles.module.css";
 
 export function Logo() {
-    return <h1 className={styles.logo}>LOGO</h1>;
+  return (
+    <div className={styles.logo}>
+      <a className={styles.logoLink} href="#">
+        <TimerIcon />
+        <span>Chronos</span>
+      </a>
+    </div>
+  );
 }
